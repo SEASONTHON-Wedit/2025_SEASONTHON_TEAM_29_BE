7ca33757defd7a15511c71e6fb12d1dbfc3d7dbe
Hyunbin Kim <taque88@naver.com>
2025-09-06 22:48:08 +0900

    [FEAT] Dress, Makeup, Studio VendorDetailsDTO 필드 작성

--- src/main/java/com/wedit/backend/api/vendor/dto/details/StudioDetailsDTO.java
+++ src/main/java/com/wedit/backend/api/vendor/dto/details/StudioDetailsDTO.java
@@ -1,6 +1,10 @@
 package com.wedit.backend.api.vendor.dto.details;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.wedit.backend.api.vendor.entity.enums.Category;
+import com.wedit.backend.api.vendor.entity.enums.StudioType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,5 +14,15 @@ import lombok.Setter;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @NoArgsConstructor
 public class StudioDetailsDTO extends VendorDetailsDTO {
-    private Integer minimumAmount;
+
+    private StudioType studioType; // OUTDOOR, NIGHT 촬영 타입
+
+    @JsonCreator
+    public StudioDetailsDTO(
+            @JsonProperty("category") Category category,
+            @JsonProperty("studioType") StudioType studioType) {
+
+        this.setCategory(category);
+        this.studioType = studioType;
+    }
 }
---
+++ src/main/java/com/wedit/backend/api/vendor/entity/enums/StudioType.java
@@ -0,0 +1,9 @@
+package com.wedit.backend.api.vendor.entity.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum StudioType {
+    OUTDOOR,
+    NIGHT,
+}
