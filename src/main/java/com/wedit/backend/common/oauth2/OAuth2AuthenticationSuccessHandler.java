ca4ecba7831e8a93c86e30c253f085108572af3f
HyunWoo9930 <ohw9930@hufs.ac.kr>
2025-09-06 15:48:01 +0900

    [CHORE] redirectURI 배포 변경

--- src/main/java/com/wedit/backend/common/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/wedit/backend/common/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -27,7 +27,7 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
 	private final MemberRepository memberRepository;
 
 	// 프론트엔드 URL (환경에 따라 설정)
-	@Value("${app.oauth2.authorized-redirect-uri:http://localhost:3000/oauth/callback}")
+	@Value("${app.oauth2.authorized-redirect-uri:https://wedit.me/oauth/callback}")
 	private String redirectUri;
 
 	@Override
