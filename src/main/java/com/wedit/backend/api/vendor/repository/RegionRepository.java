14a7c9efbda0a5faee5f1b5cd60b13d248a1bbae
Hyunbin Kim <taque88@naver.com>
2025-09-09 18:49:32 +0900

    [FEAT] RegionRepository ID 타입 변경

--- src/main/java/com/wedit/backend/api/vendor/repository/RegionRepository.java
+++ src/main/java/com/wedit/backend/api/vendor/repository/RegionRepository.java
@@ -3,7 +3,7 @@ package com.wedit.backend.api.vendor.repository;
 import com.wedit.backend.api.vendor.entity.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RegionRepository extends JpaRepository<Region, Integer> {
+public interface RegionRepository extends JpaRepository<Region, Long> {
 
 
 }
