b40ff586a242d19d58f382cda32a6324a843f1a8
Hyunbin Kim <taque88@naver.com>
2025-09-09 18:41:21 +0900

    [FEAT] Region 필드 및 지역 데이터 csv, 삽입 코드 추가

---
+++ src/main/java/com/wedit/backend/api/vendor/repository/RegionRepository.java
@@ -0,0 +1,9 @@
+package com.wedit.backend.api.vendor.repository;
+
+import com.wedit.backend.api.vendor.entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, Integer> {
+
+
+}
