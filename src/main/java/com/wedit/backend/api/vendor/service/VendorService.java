d510cce2926530a479a1f4545b0cc47f3a3cc341
Hyunbin Kim <taque88@naver.com>
2025-09-13 15:03:26 +0900

    [FEAT] 행정국역법정동코드 Region 테이블 초기화 기능 구현

--- src/main/java/com/wedit/backend/api/vendor/service/VendorService.java
+++ src/main/java/com/wedit/backend/api/vendor/service/VendorService.java
@@ -40,11 +40,13 @@ public class VendorService {
 
     @Transactional
     public Long createVendor(VendorCreateRequestDTO request) {
+        log.info("업체 생성을 시작합니다. 업체명: '{}', 타입: {}", request.getName(), request.getVendorType());
 
         Region region = regionRepository.findById(request.getRegionId())
                 .orElseThrow(() -> new NotFoundException("지역 code를 찾을 수 없습니다: " + request.getRegionId()));
 
         if (region.getLevel() != 3) {
+            log.warn("잘못된 지역 레벨입니다. 최소 단위(level 3) 지역 코드가 필요합니다. regionId: {}, level: {}", region.getId(), region.getLevel());
             throw new BadRequestException(ErrorStatus.BAD_REQUEST_REQUIRED_LEAST_REGION_CODE.getMessage());
         }
 
